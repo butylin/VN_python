f9e1290ace795072266528b241eedf217bf9f2c1
butylin <sergeibt@yandex.ru>
2017-09-05 12:42:56 -0400

    test3

--- utils/Pinger.py
+++ utils/Pinger.py
@@ -19,13 +19,13 @@ class Pinger():
 
         # TODO: check if ping failed
         print("Pinging ", site)
-        ping_str = site + " -c 5 -W 1"
-        p = subprocess.Popen(["ping", "-c6", "-w100 ", site], stdout = subprocess.PIPE)
-        ping_res = str(p.communicate()[0]).split('\\r\\n')
+        # ping_str = site + " -c 5 -W 1"
+        p = subprocess.Popen(["ping", "-c4", "-w100 ", site], stdout = subprocess.PIPE)
+        ping_res = list(str(p.communicate()[0]).split('\\r\\n'))
         print("PING RES: ",ping_res)
         ping = 0
         # parse string 64 bytes from ya.ru (87.250.250.242): icmp_seq=1 ttl=44 time=151 ms
-        for i in range(1, 6):
+        for i in range(2, 5):
              ping += int(re.search('.*time=(.*) ms.*', ping_res[i]).group(1))
         Pinger.result[site] = ping/4
 
