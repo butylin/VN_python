28f1acaeff09fa790675fa42c59eabedc3e5456c
butylin <sergeibt@yandex.ru>
2017-09-11 19:19:42 -0400

    changes

--- utils/MQTTPinger.py
+++ utils/MQTTPinger.py
@@ -1,34 +1,51 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
-import time
+import time, datetime
 import threading
 
-class MQTTPinger():
-    result = {}
-    VIN = '77'
-    ping_topic = '/control/roaming/ping/'
 
-    @staticmethod
+class MQTTPinger():
+
+    def __init__(self, broker_list, config=None):
+        self.broker_list = broker_list
+        self.config = config
+        self.result = {}
+        self.VIN = '77'
+        self.ping_topic = '/roaming/control/' + self.VIN
+
     def on_connect(self, client, userdata, flags, rc):
 
+        print("connected")
 
-    @staticmethod
-    def ping_broker(broker, topic):
-        latency_mlsec = 0
-        mqtt_client = mqtt.Client()
-        sub_topic = MQTTPinger.ping_topic + MQTTPinger.VIN
-        mqtt_client.on_connect = MQTTPinger.on_connect
-        mqtt_client.subscribe(sub_topic)
-        MQTTPinger.result[broker] = 0
-        mqtt_client.connect(broker, 1883, 60)
+        # now = datetime.datetime.now().microsecond
+        # print("Server {} start {} end {}: ".format(userdata, self.result[userdata], now))
+        # time_diff = datetime.datetime.now().microsecond - self.result[userdata]
+        # self.result[userdata] = time_diff;
+        #
+        # client.disconnect()
 
-    @staticmethod
-    def ping_brokers(brokers_list, ping_topic):
+    def ping_broker(self, broker, topic):
+        mqtt_client = mqtt.Client(userdata=broker)
+        mqtt_client.on_connect = self.on_connect
+        print("client ", mqtt_client.on_connect)
+        # mqtt_client.subscribe(sub_topic)
+        self.result[broker] = datetime.datetime.now().microsecond
+        mqtt_client.connect(broker, 1883, 10)
+        # mqtt_client.loop_start()
+
+    def ping_brokers(self):
         threads = []
-        for broker in brokers_list:
-            pingThread = threading.Thread(target=MQTTPinger.ping_broker(), args=(broker, ping_topic))
+        for broker in self.broker_list:
+            pingThread = threading.Thread(target=self.ping_broker, args=(broker, self.ping_topic))
+            print("Starting thread: ", pingThread)
             pingThread.start()
             threads.append(pingThread)
 
         for t in threads:
             t.join()
+
+        return self.result
+
+list = ['test.mosquitto.org', 'butylin-aws].ddns.net', '74.208.85.110']
+pinger = MQTTPinger(list)
+print(pinger.ping_brokers())
