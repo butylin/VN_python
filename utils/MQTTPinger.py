91033b93c2912298021ffe83e4b2dec859851da6
butylin <sergeibt@yandex.ru>
2017-09-11 20:45:20 -0400

    test3

--- utils/MQTTPinger.py
+++ utils/MQTTPinger.py
@@ -1,7 +1,9 @@
 import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
 import time, datetime
 import threading
+
+# gets list of servers and initializes MQTT connection to each of server.
+# returns dictionary {server, connect time in microseconds}
 
 
 class MQTTPinger():
@@ -13,39 +15,45 @@ class MQTTPinger():
         self.VIN = '77'
         self.ping_topic = '/roaming/control/' + self.VIN
 
+    # gets time difference between time before connect and after connecting to broker
     def on_connect(self, client, userdata, flags, rc):
+        # print("Connected with result code "+str(rc))
+        start_time = self.result[userdata]
+        end_time = datetime.datetime.now().microsecond
+        time_diff = end_time - start_time
+        # print("Server {} timediff {} start {} end {}: ".format(userdata, time_diff, start_time, end_time))
+        self.result[userdata] = time_diff
+        client.disconnect()
 
-        print("connected")
-
-        # now = datetime.datetime.now().microsecond
-        # print("Server {} start {} end {}: ".format(userdata, self.result[userdata], now))
-        # time_diff = datetime.datetime.now().microsecond - self.result[userdata]
-        # self.result[userdata] = time_diff;
-        #
-        # client.disconnect()
-
+    # creates connection to a mqtt-broker, sends broker address as 'userdata' attribute
     def ping_broker(self, broker, topic):
         mqtt_client = mqtt.Client(userdata=broker)
         mqtt_client.on_connect = self.on_connect
-        print("client ", mqtt_client.on_connect)
-        # mqtt_client.subscribe(sub_topic)
         self.result[broker] = datetime.datetime.now().microsecond
-        mqtt_client.connect(broker, 1883, 10)
-        # mqtt_client.loop_start()
+        try:
+            mqtt_client.connect(broker, 1883, 5)
+            mqtt_client.loop_forever()
+        except Exception as e:
+            print("Error connecting to {} -- {}".format(broker, e))
+            self.result[broker] = -1
 
+    # iterates through brokers-list and calls 'ping_broker()' for every broker-address in sparate thread
     def ping_brokers(self):
         threads = []
         for broker in self.broker_list:
             pingThread = threading.Thread(target=self.ping_broker, args=(broker, self.ping_topic))
             print("Starting thread: ", pingThread)
             pingThread.start()
+            # storing all started threads
             threads.append(pingThread)
 
         for t in threads:
+            # waiting for all started threads to end
             t.join()
 
         return self.result
 
-list = ['test.mosquitto.org', 'butylin-aws].ddns.net', '74.208.85.110']
-pinger = MQTTPinger(list)
-print(pinger.ping_brokers())
+
+# list = ['test.mosquitto.org', 'butylin-aws.ddns.net', '74.208.85.11', 'bumbum.com']
+# pinger = MQTTPinger(list)
+# print(pinger.ping_brokers())
