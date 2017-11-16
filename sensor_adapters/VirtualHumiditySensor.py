99ecf886165d4b8ac8c0e8117772812db4c81c31
butylin <sergeibt@yandex.ru>
2017-11-15 23:16:57 -0500

    test3

--- sensor_adapters/VirtualHumiditySensor.py
+++ sensor_adapters/VirtualHumiditySensor.py
@@ -1,9 +1,10 @@
 from sensor_adapters import Sensor
-from sensor_libs.VirtualSensor import *
+# from sensor_libs.VirtualSensor import *
+from sensor_libs import VirtualSensor
 
 class VirtualHumiditySensor(Sensor.Sensor):
 
     @classmethod
     def get_data(self):
-        sensor = VirtualSensor()
+        sensor = VirtualSensor.VirtualSensor()
         return sensor.read_humidity()
--- sensor_adapters/VirtualTemperatureSensor.py
+++ sensor_adapters/VirtualTemperatureSensor.py
@@ -1,10 +1,11 @@
 from sensor_adapters import Sensor
-from sensor_libs.VirtualSensor import *
+# from sensor_libs.VirtualSensor import *
+from sensor_libs import VirtualSensor
 
 class VirtualTemperatureSensor(Sensor.Sensor):
 
     @classmethod
     def get_data(self):
-        temp_sensor = VirtualSensor()
+        temp_sensor = VirtualSensor.VirtualSensor()
         return temp_sensor.read_temperature()
 
